4c0e8071917a9e805b1ef82a748f3af745c21d6c
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-21 16:28:10 +0200

    creat api call for new palettes

--- delocal-test/src/Api/PaletteCalls.js
+++ delocal-test/src/Api/PaletteCalls.js
@@ -1,11 +1,12 @@
 import axios from "../Axios/axios";
 
 export async function getPalettes() {
-  let response = await axios.get("/palettes/new?format=json", {
-    headers: {
-        'Access-Control-Allow-Origin' : '*',
-        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        }
-  });
+  let response = await axios.get("/http://www.colourlovers.com/api//palettes?numResults=10&format=json");
   return response.data;
 }
+
+export async function getNewPalettes(offSet) {
+  let respones = await axios.get(`https://cors-anywhere.herokuapp.com/http://www.colourlovers.com/api/palettes?numResults=10&resultOffset=${offSet}&format=json`);
+  return respones.data;
+}
+
