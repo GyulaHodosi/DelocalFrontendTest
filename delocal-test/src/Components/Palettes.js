1c325cf5887821346ec5d3a48a1472cec494a69b
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-21 16:30:19 +0200

    update palttes component with load more palettes on scroll event

--- delocal-test/src/Components/Palettes.js
+++ delocal-test/src/Components/Palettes.js
@@ -1,29 +1,47 @@
 import React, {useEffect, useState} from "react";
-import { getPalettes, getPalettesR } from "../Api/PaletteCalls";
+import { getNewPalettes, getPalettes } from "../Api/PaletteCalls";
+import { useBottomScrollListener } from 'react-bottom-scroll-listener';
 import Palette from "./Palette";
 import "./Palettes.css";
 
 export default function Palettes() {
     const [palettes, setPalettes] = useState([]);
+    const [loadCount, setLoadCount] = useState(0);
+    const [updateTime, setUpdateTime] = useState(new Date());
+
+    useBottomScrollListener(handleScroll);
+
+
+    function handleScroll(){
+        async function FetchNewPalettes() {
+            let result = await getNewPalettes(loadCount);
+            setPalettes(palettes.concat(result));
+            setLoadCount(loadCount+10);
+            setUpdateTime(new Date());
+        }
+        FetchNewPalettes();
+    };
 
     useEffect(() => {
         async function FetchPalettes() {
           let result = await getPalettes();
           setPalettes(result);
+          setLoadCount(loadCount+10);
         }
         FetchPalettes();
       }, []);
 
     return(
-        <div className="palettes-container">
+        <div id="palettes" className="palettes-container">
             <div className="header">
+            <div className="update">Last updated: {updateTime.getHours() + ":" + (updateTime.getMinutes() < 10 ? ("0" + updateTime.getMinutes()) : updateTime.getMinutes())}</div>
                 <div className="title">ColourLovers.</div>
                 <div className="sub-title">Live.</div>
-                {palettes.map(p => {
-                    return <Palette palette={p}/>
-                })}
             </div>
-            
+            {palettes.map(p => {
+                    return <Palette key={p.id} palette={p}/>
+                })}
+            <div className="instruction">Scroll down to load more palettes!</div>
         </div>
     )
 }
